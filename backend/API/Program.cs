8e306a58a997ebe5e1d2fd00c15cb7312641ef9c
Shreeansh <sarojvarshney20@gmail.com>
2021-02-16 15:27:40 +0530

    added user seed data

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -3,35 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Models;
 using Persistance;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                try {
-                    var context = services.GetRequiredService<DataContext>();
-                    context.Database.Migrate();
-                    Seed.SeedData(context);
+            using var scope = host.Services.CreateScope();
+            
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try {
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context, userManager);
 
-                    logger.LogInformation("Database up and running");
-                } catch (Exception ex) {
-                    logger.LogError("Error: ", ex);
-                }
+                logger.LogInformation("Database up and running");
+            } catch (Exception ex) {
+                logger.LogError("Error: ", ex);
             }
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
